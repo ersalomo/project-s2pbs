9863ff635a3b1bba145739ed7fa7c35ecf6a93bd
ersalomo <ersalomo2002@gmail.com>
2022-12-06 18:57:52 +0700

    membuat api untuk login dan register

--- src/backend/controller/AuthController.controller.js
+++ src/backend/controller/AuthController.controller.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const db = require('../database/index.db')
+const uuidv4 = require('uuid').v4
+const bcrypt = require('bcrypt')
 
 const AuthController = {
     index: (req,res) => {
@@ -16,15 +18,21 @@ const AuthController = {
         if(email && password) {
             const query = `select * from s2pbs where = ${email} limit 1` 
             db.query(query,(error,data)=>{
+                if(error) {
+                    return res.status(500).send({
+                        error
+                    })
+                }
                 if(data.length > 0 && data.password == password){
-                    res.status(200).send({
+                    // bcrypt.compare()
+                    return res.status(200).send({
                         status: true,
                         statusCode: res.statusCode,
                         message: 'Login success',
                         redirect : res.redirect('home/dashboard')
                     })
                 }else{
-                     res.status(422).send({
+                    return res.status(422).send({
                     status: false,
                     statusCode: res.statusCode,
                     message: 'error'
@@ -41,7 +49,38 @@ const AuthController = {
         res.end()
     },
 
-    register: () => {
+    register: async (req, res, next) => {
+        const { name, email, password,gender} = req.body
+        const id = uuidv4()
+        const nameUser = name	
+        const emailUser	= email
+        const passwordUser = await bcrypt.hash(password,10) 
+        const genderUser = 	gender
+        const createdAt = new Date().toISOString() 
+        const updatedAt = createdAt
+        return res.status(201).send({
+                status: false,
+                statusCode: res.statusCode,
+                data : [{
+                    id,
+                    nameUser,emailUser,passwordUser,genderUser,createdAt,updatedAt
+                }]
+            })
+        // make some validation
+        db.query()
+
+        if(''){
+            return res.status(201).send({
+                status: false,
+                statusCode: res.statusCode,
+                message: 'error'
+            })
+        }else{
+            return res.status(422).send({
+                status: false,
+                statusCode: 'error'
+             })
+        }
     },
 
     logout: () => {
